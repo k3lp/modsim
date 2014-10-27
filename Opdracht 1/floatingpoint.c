50be33ac6c8217e5aa43f3e4de93402583c66868
diederik555 <diederik.beker@live.nl>
2014-10-27 14:06:07 +0100

    Makefile

--- Opdracht 1/floatingpoint.c
+++ Opdracht 1/floatingpoint.c
@@ -3,20 +3,23 @@
 #include <unistd.h>
 #include <float.h>
 
-float floatRepresentation()
+void floatRepresentation()
 {
 	//size in bytes
 
 	//machine precision
 
 	//rang of valid numbers
+  
+  printf("The maximum value of float = %.10e\n", FLT_MAX);
+  printf("The minimum value of float = %.10e\n", FLT_MIN);
 	
-	return
+	//return
 } 
 
 
 
-double doubleRepresentation()
+/*double doubleRepresentation()
 {
 	//size in bytes
 
@@ -38,14 +41,13 @@ long longRepresentation()
 	
 	return
 }
+ */
 
-
-int
-main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
 	floatRepresentation();
-	doubleRepresentation();
-	longRepresentation();
+	//doubleRepresentation();
+	//longRepresentation();
 
 	return 0;
 }
