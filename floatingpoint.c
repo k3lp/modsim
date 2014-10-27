d23f581d379382440305288c95ff5e1888a1fe62
hiddehensel <hiddehensel@gmail.com>
2014-10-27 13:47:26 +0100

    hey je stinkt succes

---
+++ floatingpoint.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <float.h>
+
+float floatRepresentation()
+{
+	//size in bytes
+
+	//machine precision
+
+	//rang of valid numbers
+	
+	return
+} 
+
+
+
+double doubleRepresentation()
+{
+	//size in bytes
+
+	//machine precision
+
+	//rang of valid numbers
+	
+	return
+}
+
+
+long longRepresentation()
+{
+	//size in bytes
+
+	//machine precision
+
+	//rang of valid numbers
+	
+	return
+}
+
+
+int
+main(int argc, char* argv[])
+{
+	floatRepresentation();
+	doubleRepresentation();
+	longRepresentation();
+
+	return 0;
+}
